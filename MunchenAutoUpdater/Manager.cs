8691afa9e930782eeaf2aacbf8f8a789d5d96ac9
darbdarb <bradcooper99@gmail.com>
2023-01-03 15:20:40 +1000

    Moved some stuff to make gitignore work

--- MunchenAutoUpdater/Manager.cs
+++ MunchenAutoUpdater/Manager.cs
@@ -53,8 +53,50 @@ namespace MunchenManager
             MelonLogger.Msg(ConsoleColor.Yellow, "Starting Munchen checks...");
             {
                 if (File.Exists(Environment.CurrentDirectory + "\\MunchenFiles.zip")) File.Delete(Environment.CurrentDirectory + "\\MunchenFiles.zip");
+                if (File.Exists(MelonUtils.GameDirectory + "\\Mods\\M�nchenClient.dll"))
+                {
+                    MelonLogger.Msg("Comparing DLL's");
+                    try
+                    {
+                        var file1Path = MelonUtils.GameDirectory + "\\Mods\\M�nchenClient.dll";
+                        var file2Path = MelonUtils.GameDirectory + "\\Mods\\MünchenClient.dll";
+                        var filesAreEqual = true;
+                        using (var stream1 = File.OpenRead(file1Path))
+                        using (var stream2 = File.OpenRead(file2Path))
+                        {
+                            var sha256 = new SHA256Managed();
+                            var hash1 = sha256.ComputeHash(stream1);
+                            var hash2 = sha256.ComputeHash(stream2);
+                            if (hash1.Length != hash2.Length)
+                                filesAreEqual = false;
+                            else
+                                for (var i = 0; i < hash1.Length; i++)
+                                    if (hash1[i] != hash2[i])
+                                    {
+                                        filesAreEqual = false;
+                                        break;
+                                    }
+                        }
+
+                        if (filesAreEqual)
+                        {
+                            MelonLogger.Msg("The files have the same hash.");
+                            File.Delete(MelonUtils.GameDirectory + "\\Mods\\M�nchenClient.dll");
+                        }
+                        else
+                        {
+                            MelonLogger.Msg("The files have different hashes. Replaceing File");
+                            File.Delete(MelonUtils.GameDirectory + "\\Mods\\MünchenClient.dll");
+                            if (File.Exists(MelonUtils.GameDirectory + "\\Mods\\M�nchenClient.dll")) File.Copy(MelonUtils.GameDirectory + "\\Mods\\M�nchenClient.dll", MelonUtils.GameDirectory + "\\Mods\\MünchenClient.dll", true);
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                    }
+                }
                 WC.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36");
-                var responseString = WC.DownloadString("https://api.github.com/repos/LessUnderRated/Munchin-Client-Repasted/releases/latest");
+                var responseString = WC.DownloadString("https://api.github.com/repos/darbdarb/munchy-updater/releases/latest");
                 dynamic data = JsonConvert.DeserializeObject(responseString);
                 string downloadUrl = data.assets[0].browser_download_url;
                 //Console.WriteLine(downloadUrl);
@@ -69,7 +111,7 @@ namespace MunchenManager
                 MelonLogger.Msg(ConsoleColor.Yellow, "Getting Munchen ZIP...");
                 var wc = new WebClient();
                 wc.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36");
-                var responseString = wc.DownloadString("https://api.github.com/repos/LessUnderRated/Munchin-Client-Repasted/releases/latest");
+                var responseString = wc.DownloadString("https://api.github.com/repos/darbdarb/munchy-updater/releases/latest");
                 dynamic data = JsonConvert.DeserializeObject(responseString);
                 string downloadUrl = data.assets[0].browser_download_url;
                 MelonLogger.Msg(downloadUrl);
@@ -84,6 +126,7 @@ namespace MunchenManager
             }
 
             MelonLogger.Msg("Unpacking Munchen...");
+            if (File.Exists(MelonUtils.GameDirectory + "\\Mods\\M�nchenClient.dll")) File.Delete(MelonUtils.GameDirectory + "\\Mods\\M�nchenClient.dll");
             ManagerUtils.UnpackZip($"{MelonUtils.GameDirectory}");
             if (File.Exists(MelonUtils.GameDirectory + "\\Mods\\M�nchenClient.dll"))
             {
